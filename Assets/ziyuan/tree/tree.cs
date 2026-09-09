0030be987a09ffea5af51a9d6573bd8ff267b76d
BlackCrow699 <BlackCrow699@users.noreply.github.com>
2026-09-09 13:05:51 +0800

    Update forest scene and game assets

--- Assets/ziyuan/tree/tree.cs
+++ Assets/ziyuan/tree/tree.cs
@@ -9,7 +9,7 @@ public class tree : MonoBehaviour
         Vector3 camPos = Camera.main.transform.position;
         Vector3 targetPos = new Vector3(camPos.x, transform.position.y, camPos.z);
         transform.LookAt(targetPos);
-        transform.Rotate(-90f, -90f, 0f, Space.Self);
+        transform.Rotate(0f, 180f, 0f, Space.Self);
         
         
     }
